0f5f8be292d2169d6ae660ac1e08cd65a97f6067
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2022-03-12 14:58:22 +0100

    arrays are allocated globally

--- main.c
+++ main.c
@@ -2,21 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <omp.h>
+#include <sys/time.h>
 #include "config.h"
 #include "radix_sort.h"
 
 int* read_input_array(Config *config);
+void print_array(Config *config, int *array);
 
 int main(int argc, char *argv[])
 {
+    struct timeval start_elapsed_time, end_elapsed_time;
     Config config = init_config(argc, argv);
-    int *A = read_input_array(&config);
-    radix_sort(A, config.array_size, config.N);
-    for(int i = 0; i < config.array_size; i++)
+    if (config.nb_threads > 0)
     {
-        fprintf(config.output_file, "%d ", A[i]);
+        omp_set_num_threads(config.nb_threads);
     }
-    fprintf(config.output_file, "\n");
+    int *A = read_input_array(&config);
+    fprintf(config.output_file, "N=%d Size=%d Threads=%d Time_in_microsec=549\n", config.N, config.array_size, omp_get_max_threads());
+    print_array(&config, A);
+    gettimeofday(&start_elapsed_time, NULL);
+    radix_sort(A, config.array_size, config.N);
+    gettimeofday(&end_elapsed_time, NULL);
+    long elapsed = ((end_elapsed_time.tv_sec - start_elapsed_time.tv_sec) * 1000000) + (end_elapsed_time.tv_usec - start_elapsed_time.tv_usec);
+    printf("Elapsed time: %ld microseconds\n", elapsed);
+    print_array(&config, A);
     clean_config(&config);
     return 0;
 }
@@ -49,4 +59,13 @@ int* read_input_array(Config *config)
         i++;
     }
     return array;
+}
+
+void print_array(Config *config, int *array)
+{
+    for (int i = 0; i < config->array_size; i++)
+    {
+        fprintf(config->output_file, "%d ", array[i]);
+    }
+    fprintf(config->output_file, "\n");
 }
--- radix_sort.c
+++ radix_sort.c
@@ -2,31 +2,41 @@
 #include <math.h>
 #include "radix_sort.h"
 
+struct {
+    int *radix_sort_bits_for_i;
+    int *radix_sort_tmp_array;
+    int *split_indexes;
+    int *split_ldown;
+    int *split_lup;
+    int *split_reversed_flags;
+} radix_global_pre_allocated_heap_arrays;
+
 void permute(const int *input_array, const int *indexes_array, int *output_array, int array_size);
 void split(int *input_array, int *flags, int *output_array, int array_size);
 void prefix(const int *flags, int *output_array, int array_size);
 void suffix(const int *flags, int *output_array, int array_size);
 void revert(const int *input_array, int *output_array, int array_size);
+void pre_allocate_heap_arrays(int array_size);
+void free_heap_arrays();
 
 void radix_sort(int *array, int size, int max_value)
 {
+    pre_allocate_heap_arrays(size);
     int nb_bits = (int)log2(max_value) + 1;
     for(int i = 0; i < nb_bits; i++)
     {
-        int *bits_for_i = (int*)malloc(sizeof(int) * size);
         for(int j = 0; j < size; j++)
         {
-            bits_for_i[j] = (array[j] >> i) & 1;
+            radix_global_pre_allocated_heap_arrays.radix_sort_bits_for_i[j] = (array[j] >> i) & 1;
         }
-        int *tmp_array = (int*)malloc(sizeof(int) * size);
-        split(array, bits_for_i, tmp_array, size);
+
+        split(array, radix_global_pre_allocated_heap_arrays.radix_sort_bits_for_i, radix_global_pre_allocated_heap_arrays.radix_sort_tmp_array, size);
         for (int j = 0; j < size; ++j)
         {
-            array[j] = tmp_array[j];
+            array[j] = radix_global_pre_allocated_heap_arrays.radix_sort_tmp_array[j];
         }
-        free(bits_for_i);
-        free(tmp_array);
     }
+    free_heap_arrays();
 }
 
 void permute(const int *input_array, const int *indexes_array, int *output_array, int array_size)
@@ -39,33 +49,25 @@ void permute(const int *input_array, const int *indexes_array, int *output_array
 
 void split(int *input_array, int *flags, int *output_array, int array_size)
 {
-    int *indexes = (int*)malloc(sizeof(int) * array_size);
-    int *ldown = (int*)malloc(sizeof(int) * array_size);
-    int *lup = (int*)malloc(sizeof(int) * array_size);
-    int *reversed_flags = (int*)malloc(sizeof(int) * array_size);
-    revert(flags, reversed_flags, array_size);
-    prefix(reversed_flags, ldown, array_size);
-    suffix(flags, lup, array_size);
+    revert(flags, radix_global_pre_allocated_heap_arrays.split_reversed_flags, array_size);
+    prefix(radix_global_pre_allocated_heap_arrays.split_reversed_flags, radix_global_pre_allocated_heap_arrays.split_ldown, array_size);
+    suffix(flags, radix_global_pre_allocated_heap_arrays.split_lup, array_size);
     for(int i = 0; i < array_size; i++)
     {
-        lup[i] = (array_size + 1) - lup[i];
+        radix_global_pre_allocated_heap_arrays.split_lup[i] = (array_size + 1) - radix_global_pre_allocated_heap_arrays.split_lup[i];
     }
-    free(reversed_flags);
     for(int i = 0; i < array_size; i++)
     {
         if(flags[i] == 1)
         {
-            indexes[i] = lup[i] - 1;
+            radix_global_pre_allocated_heap_arrays.split_indexes[i] = radix_global_pre_allocated_heap_arrays.split_lup[i] - 1;
         }
         else
         {
-            indexes[i] = ldown[i] - 1;
+            radix_global_pre_allocated_heap_arrays.split_indexes[i] = radix_global_pre_allocated_heap_arrays.split_ldown[i] - 1;
         }
     }
-    permute(input_array, indexes, output_array, array_size);
-    free(indexes);
-    free(ldown);
-    free(lup);
+    permute(input_array, radix_global_pre_allocated_heap_arrays.split_indexes, output_array, array_size);
 }
 
 void prefix(const int *flags, int *output_array, int array_size)
@@ -92,4 +94,24 @@ void revert(const int *input_array, int *output_array, int array_size)
     {
         output_array[i] = !input_array[i];
     }
+}
+
+void pre_allocate_heap_arrays(int array_size)
+{
+    radix_global_pre_allocated_heap_arrays.radix_sort_bits_for_i = (int*)malloc(sizeof(int) * array_size);
+    radix_global_pre_allocated_heap_arrays.radix_sort_tmp_array = (int*)malloc(sizeof(int) * array_size);
+    radix_global_pre_allocated_heap_arrays.split_indexes = (int*)malloc(sizeof(int) * array_size);
+    radix_global_pre_allocated_heap_arrays.split_ldown = (int*)malloc(sizeof(int) * array_size);
+    radix_global_pre_allocated_heap_arrays.split_lup = (int*)malloc(sizeof(int) * array_size);
+    radix_global_pre_allocated_heap_arrays.split_reversed_flags = (int*)malloc(sizeof(int) * array_size);
+}
+
+void free_heap_arrays()
+{
+    free(radix_global_pre_allocated_heap_arrays.radix_sort_bits_for_i);
+    free(radix_global_pre_allocated_heap_arrays.radix_sort_tmp_array);
+    free(radix_global_pre_allocated_heap_arrays.split_indexes);
+    free(radix_global_pre_allocated_heap_arrays.split_ldown);
+    free(radix_global_pre_allocated_heap_arrays.split_lup);
+    free(radix_global_pre_allocated_heap_arrays.split_reversed_flags);
 }
